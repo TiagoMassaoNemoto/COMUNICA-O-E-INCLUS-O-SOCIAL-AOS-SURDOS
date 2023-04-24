87993ad8e71cc1d24cdefb5d7162ce65b0117c84
Massanemo <63130694+TiagoMassaoNemoto@users.noreply.github.com>
2023-04-24 20:55:26 -0300

    Add files via upload

--- Pratica/Teste Frame/Framenator.py
+++ Pratica/Teste Frame/Framenator.py
@@ -12,8 +12,9 @@ while cap.isOpened():
     res, frame = cap.read()
     frame_name = 'frame-%d.jpg' %(count/fps)
     #controla a quantidade de frame a salvar
+    if str(type(frame)) == "<class 'NoneType'>":
+        break
     if count % fps == 0:
         cv2.imwrite(frame_name,frame)
     count += 1
-    
 cap.release()
