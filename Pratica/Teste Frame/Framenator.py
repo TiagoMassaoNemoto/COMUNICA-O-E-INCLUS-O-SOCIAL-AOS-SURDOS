d6a5c87e01013ac3ee3248f65f7f748578e119db
Massanemo <63130694+TiagoMassaoNemoto@users.noreply.github.com>
2023-05-03 13:02:59 -0300

    Update Framenator.py
    Comentarios

--- Pratica/Teste Frame/Framenator.py
+++ Pratica/Teste Frame/Framenator.py
@@ -1,19 +1,21 @@
 import cv2
-
+#pasta onde esta o video
 video = "C:\\Users\\timas\\Videos\\Captures\\Introdução.mkv"
-
+#variavel com o video
 cap = cv2.VideoCapture(video)
 count = 0
-#"controla a quantidade de frame a salvar"
+#"variavel que controla a quantidade de frame a salvar"
 fps = 10
 
 while cap.isOpened():
-
+#separado os frames do video
     res, frame = cap.read()
-    frame_name = 'frame-%d.jpg' %(count/fps)
     #controla a quantidade de frame a salvar
+    frame_name = 'frame-%d.jpg' %(count/fps)
+    #verifica se ainda tem frame para salvar
     if str(type(frame)) == "<class 'NoneType'>":
         break
+    #salva os frames
     if count % fps == 0:
         cv2.imwrite(frame_name,frame)
     count += 1
